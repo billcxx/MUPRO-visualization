64e3ebd5dbed6f419e7b7c2808fd2084b7c89f5b
cxx <0001001bill@gmail.com>
2017-05-28 14:13:53 -0400

    Nothing really changed
    the liblink and tool is not working any more, use the qt macdeploy tool
    instead. I think this is due to no property info.plist is generated

--- liblink.sh
+++ liblink.sh
@@ -44,7 +44,7 @@ lib=("libvtkViewsQt" "libvtkIOLegacy" "libvtkGUISupportQt" "libvtkRenderingOpenG
 
 
 
-install_name_tool -add_rpath /Users/cxx/Qt/5.5/clang_64/lib/ ${app}.app/Contents/MacOS/${app}
+install_name_tool -add_rpath /Users/cxx/Qt/5.7/clang_64/lib/ ${app}.app/Contents/MacOS/${app}
 name="libvtkRenderVolumeOpenGL"
 install_name_tool -change ${name}-7.0.1.dylib /usr/local/vtk7-share/lib/libvtkRenderingVolumeOpenGL-7.0.1.dylib ${app}.app/Contents/MacOS/${app}
 for name in ${lib[@]}; do
